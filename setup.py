02aa5dca0f2369210a6a46387526ebcafe27c015
fionapigott <fpigott@twitter.com>
2017-06-01 14:48:04 -0600

    fix typos

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='tweet_parser',
+        packages=find_packages(),
+        scripts=[],
+        version='0.1',
+        license='MIT',
+        author='Fiona Pigott',
+        author_email='fpigott@twitter.com',
+        description="Tools for Tweet parsing", 
+        url='https://github.com/fionapigott/tweet-parser',
+        install_requires=[]
+        )
