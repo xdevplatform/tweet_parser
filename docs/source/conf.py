8ca2d226bff223341c29785bdf328b83de1f61c3
Aaron Gonzales <agonzales@twitter.com>
2017-08-10 15:08:20 -0600

    changed theme to bootstrap and autoincluded readme from root

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import sphinx_bootstrap_theme
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('.'))
 
@@ -42,9 +43,12 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
+
+source_parsers = {
+   '.md': 'recommonmark.parser.CommonMarkParser',
+}
 source_suffix = ['.rst', '.md']
-# source_suffix = '.rst'
+
 
 # The master toctree document.
 master_doc = 'index'
@@ -87,7 +91,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
