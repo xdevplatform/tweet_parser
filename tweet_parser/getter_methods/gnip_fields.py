ad9c9701a00abba7de4904dcd45cc1c86ad54c37
fionapigott <fpigott@twitter.com>
2017-08-17 17:42:17 -0600

    fix for matching_rules for original format

--- tweet_parser/getter_methods/gnip_fields.py
+++ tweet_parser/getter_methods/gnip_fields.py
@@ -1,3 +1,6 @@
+from tweet_parser.tweet_checking import is_original_format
+
+
 def get_matching_rules(tweet):
     """
     Retrieves the matching rules for a tweet with a gnip field enrichment.
@@ -6,10 +9,16 @@ def get_matching_rules(tweet):
         tweet (Tweet): the tweet
 
     Returns:
-        list: potential ``[{"tag": "rule"}]`` pairs from standard rulesets or
-        None if no rules or no field gnip is found.
+        list: potential ``[{"tag": "user_tag", "value": "rule_value"}]`` 
+        pairs from standard rulesets or None if no rules or no
+        matching_rules field is found. \n
+        More information on this value at:
+        http://support.gnip.com/enrichments/matching_rules.html
 
     """
-    gnip = tweet.get("gnip")
-    rules = gnip.get("matching_rules") if gnip else None
+    if is_original_format(tweet):
+        rules = tweet.get("matching_rules")
+    else:
+        gnip = tweet.get("gnip")
+        rules = gnip.get("matching_rules") if gnip else None
     return rules
