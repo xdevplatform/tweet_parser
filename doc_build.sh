b253723e9012f92bdaf51da089f6dc604570705d
Aaron Gonzales <agonzales@twitter.com>
2017-11-06 11:24:58 -0700

    updating to remove more stale files from doc builds

--- doc_build.sh
+++ doc_build.sh
@@ -23,13 +23,13 @@ git pull origin gh-pages
 rm -r *.html *.js
 touch .nojekyll
 git checkout $BRANCH_NAME docs tweet_parser README.rst
-# need to do this step because the readme will be overwritten
-# pandoc -i README.md -o docs/source/README.rst
+
+
 mv docs/* .
 cp README.rst source/README.rst
 make html
 mv -fv build/html/* ./
-rm -r tweet_parser docs build Makefile source __pycache__/
+rm -r tweet_parser docs build Makefile source __pycache__/ _sources/ _static/
 echo "--------------------------------------------------------------------"
 echo " docs built; please review these changes and then run the following:"
 echo "--------------------------------------------------------------------"
