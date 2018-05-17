xdevplatform/tweet_parser

commit 5f93c5bcefb309c9675e37e04e45040e9d73a5de

attributes for retweets, favs, and quotes (#32)
* added count attributes for retweets, favs, and quotes

* bumped minor version

* synatx highlighting

* fixed typo in error message

setup.py
@@ -7,7 +7,7 @@ setup(name='tweet_parser',
       long_description=open('README.rst', 'r').read(),
       author_email='fpigott@twitter.com',
       license='MIT',
-      version='1.11.0',
+      version='1.12.0',
       packages=find_packages(),
       scripts=["tools/parse_tweets.py"],
       install_requires=[],

tweet_parser/getter_methods/tweet_counts.py
@@ -1,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""Tweet counts and related attributes
+
+This module holds attributes related to basic counts on tweets, such as
+retweets, favs, and quotes. It is unlikely to be extended.
+"""
+
+from tweet_parser.tweet_checking import is_original_format
+
+def get_retweet_count(tweet):
+    """
+    Gets the retweet count for this tweet.
+
+    Example:
+        >>> from tweet_parser.getter_methods.tweet_counts import get_retweet_count
+        >>> tweet = {'created_at': '2017-21-23T15:21:21.000Z',
+        ...          'id_str': '2382763597'
+        ...          'retweet_count': 2}
+        2
+    """
+    return tweet.get("retweet_count", 0)
+
+
+def get_favorite_count(tweet):
+    """
+    Gets the favorite count for this tweet.
+
+    Example:
+        >>> from tweet_parser.getter_methods.tweet_counts import get_retweet_count
+        >>> tweet = {'created_at': '2017-21-23T15:21:21.000Z',
+        ...          'id_str': '2382763597'
+        ...          'favorite_count': 2}
+        2
+    """
+    return tweet.get("favorite_count", 0)
+
+
+def get_quote_count(tweet):
+    """
+    Gets the quote count for this tweet.
+
+    Example:
+        >>> from tweet_parser.getter_methods.tweet_counts import get_retweet_count
+        >>> tweet = {'created_at': '2017-21-23T15:21:21.000Z',
+        ...          'id_str': '2382763597'
+        ...          'quote_count': 2}
+        2
+    """
+    if is_original_format(tweet):
+        return tweet.get("quote_count", 0)
+    else:
+        print("quote counts are only available in original format")
+        raise KeyError

tweet_parser/tweet.py
@@ -3,7 +3,7 @@ import datetime
 from tweet_parser.lazy_property import lazy_property
 from tweet_parser.tweet_parser_errors import NotATweetError
 from tweet_parser import tweet_checking
-from tweet_parser.getter_methods import tweet_date, tweet_user
+from tweet_parser.getter_methods import tweet_date, tweet_user, tweet_counts
 from tweet_parser.getter_methods import tweet_text, tweet_geo, tweet_links
 from tweet_parser.getter_methods import tweet_entities, tweet_embeds
 from tweet_parser.getter_methods import gnip_fields, tweet_generator, tweet_reply
@@ -633,3 +633,30 @@ class Tweet(dict):
             str: value returned by calling `tweet_reply.get_in_reply_to_status_id` on `self`
         """
         return tweet_reply.get_in_reply_to_status_id(self)
+
+    @lazy_property
+    def favorite_count(self):
+        """
+        The number of favorites that this tweet has received *at the time of
+        retrieval*. If a tweet is obtained from a live stream, this will likely
+        be 0.
+        """
+        return tweet_counts.get_favorite_count(self)
+
+    @lazy_property
+    def quote_count(self):
+        """
+        The number of tweets that this tweet has been quoted in *at the time of
+        retrieval*. If a tweet is obtained from a live stream, this will likely
+        be 0.
+        """
+        return tweet_counts.get_quote_count(self)
+
+    @lazy_property
+    def retweet_count(self):
+        """
+        The number of times this tweet has been retweeted *at the time of
+        retrieval*. If a tweet is obtained from a live stream, this will likely
+        be 0.
+        """
+        return tweet_counts.get_retweet_count(self)

commit 6a5f9b37d8cc467d3a7d304a1cafc00f146598ae

Remove timezone (#36)
* remove the utc_timezone getters from the obj, had to fix some tests that appear to have been broken since #32

* fix broken doctests

* version

setup.py
@@ -7,7 +7,7 @@ setup(name='tweet_parser',
       long_description=open('README.rst', 'r').read(),
       author_email='fpigott@twitter.com',
       license='MIT',
-      version='1.13.0',
+      version='1.13.1',
       packages=find_packages(),
       scripts=["tools/parse_tweets.py"],
       install_requires=[],

tweet_parser/getter_methods/tweet_counts.py
@@ -6,48 +6,87 @@ retweets, favs, and quotes. It is unlikely to be extended.
 """
 
 from tweet_parser.tweet_checking import is_original_format
+from tweet_parser.tweet_parser_errors import NotAvailableError
 
 def get_retweet_count(tweet):
     """
     Gets the retweet count for this tweet.
 
+    Args:
+        tweet (Tweet): A Tweet object (or a dictionary)
+
+    Returns:
+        int: The number of times the Tweet has been retweeted
+
     Example:
         >>> from tweet_parser.getter_methods.tweet_counts import get_retweet_count
         >>> tweet = {'created_at': '2017-21-23T15:21:21.000Z',
-        ...          'id_str': '2382763597'
+        ...          'id_str': '2382763597',
         ...          'retweet_count': 2}
+        >>> get_retweet_count(tweet)
         2
+
+        >>> activity_streams_tweet = {'postedTime': '2017-05-24T20:17:19.000Z',
+        ...                           'retweetCount': 3}
+        >>> get_retweet_count(activity_streams_tweet)
+        3
     """
-    return tweet.get("retweet_count", 0)
+    if is_original_format(tweet):
+        return tweet.get("retweet_count", 0)
+    else:
+        return tweet.get("retweetCount", 0)
 
 
 def get_favorite_count(tweet):
     """
     Gets the favorite count for this tweet.
 
+    Args:
+        tweet (Tweet): A Tweet object (or a dictionary)
+
+    Returns:
+        int: The number of times the Tweet has been favorited
+
     Example:
-        >>> from tweet_parser.getter_methods.tweet_counts import get_retweet_count
+        >>> from tweet_parser.getter_methods.tweet_counts import get_favorite_count
         >>> tweet = {'created_at': '2017-21-23T15:21:21.000Z',
-        ...          'id_str': '2382763597'
+        ...          'id_str': '2382763597',
         ...          'favorite_count': 2}
+        >>> get_favorite_count(tweet)
         2
+        
+        >>> activity_streams_tweet = {'postedTime': '2017-05-24T20:17:19.000Z',
+        ...                           'favoritesCount': 3}
+        >>> get_favorite_count(activity_streams_tweet)
+        3
     """
-    return tweet.get("favorite_count", 0)
+    if is_original_format(tweet):
+        return tweet.get("favorite_count", 0)
+    else:
+        return tweet.get("favoritesCount", 0)
 
 
 def get_quote_count(tweet):
     """
-    Gets the quote count for this tweet.
+    Gets the quote count for this tweet. \n 
+    Note that this is unavailable in activity-streams format
+
+    Args:
+        tweet (Tweet): A Tweet object (or a dictionary)
+
+    Returns:
+        int: The number of times the Tweet has been quoted
+        or for activity-streams raise a NotAvailableError
 
     Example:
-        >>> from tweet_parser.getter_methods.tweet_counts import get_retweet_count
+        >>> from tweet_parser.getter_methods.tweet_counts import get_quote_count
         >>> tweet = {'created_at': '2017-21-23T15:21:21.000Z',
-        ...          'id_str': '2382763597'
+        ...          'id_str': '2382763597',
         ...          'quote_count': 2}
+        >>> get_quote_count(tweet)
         2
     """
     if is_original_format(tweet):
         return tweet.get("quote_count", 0)
     else:
-        print("quote counts are only available in original format")
-        raise KeyError
+        raise NotAvailableError("Quote counts are only available in original format")

tweet_parser/getter_methods/tweet_user.py
@@ -144,45 +144,6 @@ def get_bio(tweet):
         return tweet["actor"].get("summary", "")
 
 
-def get_utc_offset(tweet):
-    """
-    Get the utc offset (if it's available) of the user who posted the Tweet
-
-    Args:
-        tweet (Tweet): A Tweet object (or a dictionary)
-
-    Returns:
-        int: a signed integer indicating the number of seconds offset of the
-        user's home timezone from UTC time
-
-    Example:
-        >>> from tweet_parser.getter_methods.tweet_user import get_utc_offset
-        >>> original_format_dict = {
-        ...             "created_at": "Wed May 24 20:17:19 +0000 2017",
-        ...             "user":
-        ...              {"utc_offset": -21600}
-        ...            }
-        >>> get_utc_offset(original_format_dict)
-        -21600
-
-        >>> activity_streams_format_dict = {
-        ...             "postedTime": "2017-05-24T20:17:19.000Z",
-        ...             "actor":
-        ...              {"utcOffset": "-21600"}
-        ...             }
-        >>> get_utc_offset(activity_streams_format_dict)
-        -21600
-    """
-
-    if is_original_format(tweet):
-        return tweet["user"]["utc_offset"]
-    else:
-        if tweet["actor"]["utcOffset"] is not None:
-            return int(tweet["actor"]["utcOffset"])
-        else:
-            return None
-
-
 def get_follower_count(tweet):
     """
     Get the number of followers that the user has

tweet_parser/tweet.py
@@ -175,20 +175,6 @@ class Tweet(dict):
         return tweet_user.get_bio(self)
 
     @lazy_property
-    def utc_offset(self):
-        """
-        The utc offset (if it's available) of the timezone of the user who
-        posted the Tweet
-
-        Returns:
-            str: a signed integer indicating the number of seconds offset of the
-            user's home timezone from UTC time.
-            value returned by calling `tweet_user.get_utc_offset` on `self`
-
-        """
-        return tweet_user.get_utc_offset(self)
-
-    @lazy_property
     def follower_count(self):
         """
         The number of followers that the author of the Tweet has
@@ -640,6 +626,9 @@ class Tweet(dict):
         The number of favorites that this tweet has received *at the time of
         retrieval*. If a tweet is obtained from a live stream, this will likely
         be 0.
+
+        Returns:
+            int: value returned by calling `tweet_counts.get_favorite_count` on `self`
         """
         return tweet_counts.get_favorite_count(self)
 
@@ -649,6 +638,11 @@ class Tweet(dict):
         The number of tweets that this tweet has been quoted in *at the time of
         retrieval*. If a tweet is obtained from a live stream, this will likely
         be 0.
+        This raises a NotAvailableError for activity-streams format
+        
+        Returns:
+            int: value returned by calling `tweet_counts.get_quote_count` on `self` 
+            or raises NotAvailableError
         """
         return tweet_counts.get_quote_count(self)
 
@@ -658,5 +652,8 @@ class Tweet(dict):
         The number of times this tweet has been retweeted *at the time of
         retrieval*. If a tweet is obtained from a live stream, this will likely
         be 0.
+        
+        Returns:
+            int: value returned by calling `tweet_counts.get_retweet_count` on `self` 
         """
         return tweet_counts.get_retweet_count(self)

tweet_parser/tweet_keys.py
@@ -130,7 +130,6 @@ original_format_superset_keys = {
     'quoted_status user time_zone',
     'quoted_status user translator_type',
     'quoted_status user url',
-    'quoted_status user utc_offset',
     'quoted_status user verified',
     'quoted_status_id',
     'quoted_status_id_str',
@@ -220,7 +219,6 @@ original_format_superset_keys = {
     'retweeted_status user time_zone',
     'retweeted_status user translator_type',
     'retweeted_status user url',
-    'retweeted_status user utc_offset',
     'retweeted_status user verified',
     'source',
     'text',

test/test_tweet_parser.py
@@ -56,7 +56,7 @@ class TestTweetMethods(unittest.TestCase):
                     if attr == "text":
                         orig = orig[0:100]
                         acti = acti[0:100]
-                    if attr not in ["poll_options","in_reply_to_user_id"]:  # will raise an error in activity streams
+                    if attr not in ["poll_options","in_reply_to_user_id","quote_count"]:  # will raise an error in activity streams
                         self.assertEqual(orig, acti)
 
     def test_bad_payloads(self):